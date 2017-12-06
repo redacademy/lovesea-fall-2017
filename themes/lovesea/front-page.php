8df3dbed1fcbf626dc87cba0d687bbfd36c12c7a
btrischuk <b-trischuk@hotmail.com>
2017-12-06 10:21:25 -0800

    move front page content to front-page.php

--- themes/lovesea/front-page.php
+++ themes/lovesea/front-page.php
@@ -14,6 +14,30 @@ get_header(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 
+				<div id="storyteller" class="storyteller">
+	<h1>We are story tellers.</h1>
+	<p>With a photojournalistic style, we capture both the story and romance of your wedding.</p>
+	<button class="browse-wedding"></button> 
+</div>
+
+<div class="observer">
+	<h1>We are observers.</h1>
+	<p>We work in the background, capturing even the tiniest of details, to tell your story.</p>
+	<button class="aboutus"></button>
+</div>
+
+<div class="friend">
+	<h1>We are your friend.</h1>
+	<p>We get to know you and your story to perfectly document what’s important to you.</p>
+	<button class="connect"></button>
+</div>
+
+<div class="instagram-carousel-container"> 
+	<h2>follow along on <span class="instagram"> Instagram</span> </h2>
+	<div class="instagram-carousel">
+	</div>
+</div>
+
 			<?php endwhile; // End of the loop. ?>
 
 		</main><!-- #main -->
