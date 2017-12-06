fbbf937a86901cae33fc83bd9837f12de77ebeaa
Dylan Kocsis <Dylan.Kocsis@queensu.ca>
2017-12-05 16:11:59 -0800

    update about.php file to allow for custom template.

--- themes/lovesea/page-templates/about.php
+++ themes/lovesea/page-templates/about.php
@@ -1,23 +1,23 @@
 <?php
-/**
- * The template for displaying all pages.
- *
- * @package RED_Starter_Theme
- */
+/** Template Name: About Page */
 
 get_header(); ?>
+  <div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+      <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <header class="entry-header custom-hero">
+          <div class ="container">
+            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+          </div>
+        </header>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-
-
+        <div class="container">
+          <div class="entry-content">
+            <h2>Our Story</h2>
+						<h3>who we are</h3>
+          </div>
+        </div>
+      </article>
+    </main>
+  </div>
 <?php get_footer(); ?>
