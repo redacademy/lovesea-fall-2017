774f5e1ea9ba3d9b3eaa94a66f003d1e4d7cb726
Dylan Kocsis <Dylan.Kocsis@queensu.ca>
2017-12-07 13:52:18 -0800

    Styling of About page

--- themes/lovesea/page-templates/about.php
+++ themes/lovesea/page-templates/about.php
@@ -6,27 +6,24 @@ get_header(); ?>
     <main id="main" class="site-main" role="main">
       <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
         <header class="entry-header custom-hero">
-          <div class ="container">
-            <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-          </div>
         </header>
-
         <div class="container">
           <div class="entry-content">
-            <div class="about-sub-pages">
-              <h2><?php
+          <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+            <ul class="about-sub-pages">
+              <li><?php
               $props = CFS()->get_field_info( 'who_we_are' );
               echo $props['label'];
-              ?></h2>
-               <h2><?php
+              ?></li>
+               <li><?php
               $props = CFS()->get_field_info( 'what_we_do' );
               echo $props['label'];
-              ?></h2>
-               <h2><?php
+              ?></li>
+               <li><?php
               $props = CFS()->get_field_info( 'what_to_expect' );
               echo $props['label'];
-              ?></h2>
-            </div>
+              ?></li>
+            </ul>
               <div class="who-we-are-wrapper">
                 <div class="who-are-we-image"><img src="<?php
                 echo CFS()->get( 'who_we_are_image' ); 
