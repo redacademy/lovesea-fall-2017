8a91b22abba9223d1b53727084b9d6d32b37dde9
btrischuk <b-trischuk@hotmail.com>
2017-12-10 16:50:36 -0800

    Add scroll arrow on front page. Create variable for main color  border and add border to Instagram header

--- themes/lovesea/front-page.php
+++ themes/lovesea/front-page.php
@@ -11,6 +11,9 @@ get_header(); ?>
 	
 	<img class="front-center-logo"  src=<?php echo get_template_directory_uri() . '/images/lovesea_logo_monogram.svg' ?>>
 	<img class="front-center-text"  src=<?php echo get_template_directory_uri() . '/images/lovesea-logo-text.svg' ?>>
+	<div class="scroll-arrow">
+					<button class="scroll"><h1><a href="#storyteller">╲╱</h1></button>
+							</div>
 
 </div>
 
@@ -19,40 +22,75 @@ get_header(); ?>
 
 		<!-- <img class="front-center-logo" src='/images/Lovesea_logo_text.svg' alt="cool" > -->
 
+		<?php if ( has_post_thumbnail() && is_front_page() ) : ?>
+   		<?php the_post_thumbnail( 'large' ); ?>
+   	<?php endif; ?>
+
+
+		<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
+
+					<?php if ( red_starter_categorized_blog() ) : // Only show the widget if site has multiple categories. ?>
+					<div class="widget widget_categories">
+						<h2 class="widget-title"><?php echo esc_html( 'Most Used Categories' ); ?></h2>
+						<ul>
+						<?php
+							wp_list_categories( array(
+								'orderby'    => 'count',
+								'order'      => 'DESC',
+								'show_count' => 1,
+								'title_li'   => '',
+								'number'     => 10,
+							) );
+						?>
+						</ul>
+					</div><!-- .widget -->
+					<?php endif; ?>
+
+
+
+
+
 			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+
+
+
+
+			
+			
 
 				<section class="introduction-container">
+				<?php the_widget( 'WP_Widget_Recent_Posts' ); ?>
 
 					<div id="storyteller" class="storyteller">
 						<h1>We are story tellers.</h1>
 						<p>With a photojournalistic style, we capture both the story and romance of your wedding.</p>
-						<button class="browse-weddings">Browse Weddings</button> 
+						<button class="browse-weddings"><a href = "<?php echo esc_url(get_permalink());?>">Browse Weddings</a></button> 
+						
 					</div>
 
 					<div class="observer">
 						<h1>We are observers.</h1>
 						<p>We work in the background, capturing even the tiniest of details, to tell your story.</p>
-						<button class="about-us">About Us</button>
+						<button class="about-us"><a href = "<?php echo esc_url(get_permalink());?>">About Us</a></button>
 					</div>
 
 					<div class="friend">
 						<h1>We are your friend.</h1>
 						<p>We get to know you and your story to perfectly document what’s important to you.</p>
-						<button class="connect">Let's Talk</button>
+						<button class="connect"><a href = "http://localhost:3000/lovesea/connect/">Let's Talk</a></button>
 					</div>
 
 
-				<div class="instagram-carousel-container"> 
-					<h2>follow along on <span class="instagram"> Instagram</span> </h2>
+					<div class="instagram-carousel-container"> 
+						<h2>follow along on <span class="instagram"> Instagram</span> </h2>
 
-					<div class="owl-carousel">
-	
+						<div class="owl-carousel">
+		
+						</div>
+
+
 					</div>
-
-
-				</div>
 				</section>
 
 			<?php endwhile; // End of the loop. ?>
--- themes/lovesea/functions.php
+++ themes/lovesea/functions.php
@@ -89,6 +89,8 @@ function lovesea() {
 	wp_enqueue_script( 'jquery');
 	wp_enqueue_script( 'scripts', get_template_directory_uri() . '/build/js/scripts.min.js', array('jquery'), false);
 
+	// wp_enqueue_scripts('  <script src="js/smoothscroll.js"></script>');
+
 	wp_enqueue_script( 'lovesea-spectra', 'https://cdnjs.cloudflare.com/ajax/libs/spectragram/1.0.3/spectragram.min.js', array('jquery'), false, false );
 	wp_enqueue_script( 'lovesea-owl', 'https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.js', array('jquery', 'lovesea-spectra'), false, true );
 	
