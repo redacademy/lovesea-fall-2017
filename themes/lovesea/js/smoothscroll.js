34f13638e4475f605e301a3b2a06baddccaca645
btrischuk <b-trischuk@hotmail.com>
2017-12-11 14:31:53 -0800

    Change == to === in smooth scroll

--- themes/lovesea/js/smoothscroll.js
+++ themes/lovesea/js/smoothscroll.js
@@ -9,9 +9,9 @@ $('a[href*="#"]')
 .click(function(event) {
   // On-page links
   if (
-    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') 
     && 
-    location.hostname == this.hostname
+    location.hostname === this.hostname
   ){
     // Figure out element to scroll to
     var target = $(this.hash);
