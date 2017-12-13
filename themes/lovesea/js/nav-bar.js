c5897647e14c196d927f88dea85beb96f596f7ef
btrischuk <b-trischuk@hotmail.com>
2017-12-13 11:18:04 -0800

    Remove excess code from nav-bar.js. Add transition to dropdown menu.

--- themes/lovesea/js/nav-bar.js
+++ themes/lovesea/js/nav-bar.js
@@ -1,47 +1,11 @@
 (function ($) {
-  // jQuery(document).ready(function() {
-  //   jQuery('.toggle-nav').click(function(e) {
-  //     jQuery(this).toggleClass('menu-items');
-  //     jQuery('.menu ul').toggleClass('menu-items');
-
-  //     e.preventDefault();
-  //   });
-  // });
-
-    
-  
-
-    
-    // $('#toggle-menu-button').on('blur', function (){
-    //   $('.menulist').toggle()
-    // });
-  
 
     $('.toggle-menu-button').on('click', function () {
       $('.menu-list').toggleClass('toggle-nav');
     });
 
-    // $('.burger').click(function () {
-    //   $(this).toggleClass('open');
-    // });
-
-    // $( '.toggle')
-
-    //   if ($(document).scrollTop() > 3) {
-    //     $('.front-center-logo').addClass('shrink');
-    //   } else {
-    //     $('.front-center-logo').removeClass('shrink');
-    //   }
-    // });
-
-    // $('.hamburger').on('click', function () {
-      
-    //   $('.menu').toggleClass('open');
-      
-    //   });
-      
-    //   $( '.menu a' ).on("click", function(){
-    //   $('.menu').hide();
-    //   });
+    $('.toggle-menu-button').on('blur', function () {
+      $('.menu-list').toggleClass('toggle-nav');
+    });
     
 })(jQuery);
